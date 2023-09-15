5e3561f57cee75c023a607cfb639485d5fdcffbb
michareichmann <micha.reichmann@gmail.com>
2023-09-15 20:57:19 +0200

    add option to pause a mine and run update in thread

--- src/mine.py
+++ src/mine.py
@@ -3,7 +3,14 @@ from datetime import datetime
 from typing import List
 
 from src.resource import *
-from utils.helpers import Dir, load_pickle, print_table
+from utils.helpers import Dir, load_pickle, print_table, info
+from threading import Timer
+
+
+class RepeatTimer(Timer):
+    def run(self):
+        while not self.finished.wait(self.interval):
+            self.function(*self.args, **self.kwargs)
 
 
 def now():
@@ -12,7 +19,7 @@ def now():
 
 class Mine:
 
-    def __init__(self, resource: Resource, dep_size: int, extra_time, prod_time=None, lvl=1):
+    def __init__(self, resource: Resource, dep_size: int, extra_time, prod_time=None, lvl=1, paused=False):
 
         self.Resource = resource * dep_size
         self.ExtraTime = duration(extra_time)
@@ -23,10 +30,7 @@ class Mine:
         self.T = now()
         self.EndOfLife = self.end_of_life
 
-    def set_deposit(self, s):
-        self.T = now()
-        self.Resource.update(s)
-        self.EndOfLife = self.end_of_life
+        self.Paused = paused
 
     def __repr__(self):
         return f'{self.Resource} mine containing {self.Resource!r}'
@@ -34,8 +38,20 @@ class Mine:
     def __lt__(self, other: 'Mine'):
         return self.EndOfLife < other.EndOfLife
 
-    def dep_str(self):
-        return f' containing {self.Resource!r}' if self.Resource.N > 1 else ''
+    def set_lvl(self, lvl):
+        self.Level = lvl
+        self.EndOfLife = self.end_of_life
+
+    def set_deposit(self, s):
+        self.T = now()
+        self.Resource.update(s)
+        self.EndOfLife = self.end_of_life
+
+    def add_deposit(self, n):
+        self.Resource += n
+        new_eol = self.end_of_life
+        info(f'You added {new_eol - self.EndOfLife} of life time')
+        self.EndOfLife = new_eol
 
     @property
     def end_of_life(self):
@@ -43,13 +59,20 @@ class Mine:
 
     @property
     def time_left(self):
-        return self.EndOfLife - now()
+        return None if self.Paused else self.EndOfLife - now()
 
     def update(self):
         tdiff = now() - self.T
         n_cycles = int(tdiff / self.ProdTime)
         self.T += n_cycles * self.ProdTime
-        self.Resource -= n_cycles * self.Level
+        if not self.Paused:
+            self.Resource -= n_cycles * self.Level
+
+    def pause(self):
+        self.Paused = True
+
+    def activate(self):
+        self.Paused = False
 
 
 class Mines:
@@ -60,12 +83,18 @@ class Mines:
     def __init__(self):
         self.L = self.load()
 
+        self.Timer = RepeatTimer(5, self.update)
+        self.Timer.start()
+
     def __getitem__(self, item):
         return self.L[item]
 
     def __repr__(self):
         mine_str = '\n'.join(f'  {mine}' for mine in self)
         return f'Registered mines ({self.size}): \n{mine_str}'
+
+    def __del__(self):
+        self.save()
 
     @property
     def size(self):
@@ -81,16 +110,13 @@ class Mines:
 
     def register(self, mine: Mine):
         self.L.append(mine)
-        self.save()
 
     def remove(self, i):
         print(f'removed {self.L.pop(i)} from the mines')
-        self.save()
 
     def update(self):
         for mine in self:
             mine.update()
-        self.save()
 
     def print(self):
         rows = [[m.Resource, m.Level, m.Resource.N, str(m.time_left)] for m in self.L]
@@ -99,25 +125,25 @@ class Mines:
 
 class CoalMine(Mine):
 
-    def __init__(self, dep_size, extra_time, lvl=1):
-        super().__init__(Coal, dep_size, extra_time, prod_time=90, lvl=lvl)
+    def __init__(self, dep_size, extra_time, lvl=1, paused=False):
+        super().__init__(Coal, dep_size, extra_time, prod_time=90, lvl=lvl, paused=paused)
 
 
 class CopperMine(Mine):
 
-    def __init__(self, dep_size, extra_time, lvl=1):
-        super().__init__(CopperOre, dep_size, extra_time, lvl=lvl)
+    def __init__(self, dep_size, extra_time, lvl=1, paused=False):
+        super().__init__(CopperOre, dep_size, extra_time, lvl=lvl, paused=paused)
 
 
 class IronMine(Mine):
 
-    def __init__(self, dep_size, extra_time, lvl=1):
-        super().__init__(IronOre, dep_size, extra_time, lvl=lvl)
+    def __init__(self, dep_size, extra_time, lvl=1, paused=False):
+        super().__init__(IronOre, dep_size, extra_time, lvl=lvl, paused=paused)
 
 
 class GoldMine(Mine):
 
-    def __init__(self, dep_size, extra_time, lvl=1):
-        super().__init__(GoldOre, dep_size, extra_time, lvl=lvl)
+    def __init__(self, dep_size, extra_time, lvl=1, paused=False):
+        super().__init__(GoldOre, dep_size, extra_time, lvl=lvl, paused=paused)
 
 

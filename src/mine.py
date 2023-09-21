db15bcdaf8617ab56eb27a283102a8124de10011
michareichmann <micha.reichmann@gmail.com>
2023-09-21 20:55:45 +0200

    don't reload after adding mine

--- src/mine.py
+++ src/mine.py
@@ -1,7 +1,9 @@
 import pickle
 from datetime import datetime
-from typing import List
 from functools import partial
+from typing import List
+
+import numpy as np
 
 from src.resource import *
 from utils.helpers import Dir, load_pickle, print_table, info
@@ -15,33 +17,49 @@ class Mine:
 
     def __init__(self, resource: Resource, dep_size: int, extra_time, prod_time=None, lvl=1, paused=False, speed=1):
 
+        # FEATURES
         self.Resource = resource * int(dep_size)
         self.ExtraTime = duration(extra_time)
         self._ProdTime = resource.ProdTime if prod_time is None else duration(prod_time)
         self.Speed = speed
         self.ProdTime = self._ProdTime * speed + self.ExtraTime
         self.Level = int(lvl)
+        self.Paused = bool(paused)
 
         self.T = now()
-        self.EndOfLife = self.end_of_life
-
-        self.Paused = bool(paused)
 
     def __str__(self):
         return f'{self.Resource} mine'
 
     def __repr__(self):
-        return f'{self} containing {self.Resource!r}'
+        return f'Lvl {self.Level} {self} containing {self.Resource!r}'
 
     def __lt__(self, other: 'Mine'):
-        return True if other.Paused else False if self.Paused else self.EndOfLife < other.EndOfLife
+        return other.Paused if self.Paused != other.Paused else self.time_left < other.time_left
+
+    def update(self):
+        tdiff = now() - self.T
+        self.T += tdiff
+        if not self.Paused:
+            n_cycles = int(tdiff / self.ProdTime)
+            self.Resource -= n_cycles * self.Level
+
+    @property
+    def time_left(self):
+        return np.ceil(self.Resource.N / self.Level) * self.ProdTime
+
+    @property
+    def time_left_str(self):
+        return f'({self.time_left})' if self.Paused else str(self.time_left)
     
-    def time(self, n):
-        return n / self.Level * self.ProdTime
+    def extra_time(self, n):
+        rest = self.Resource.N % self.Level
+        cycles = int((rest + n) / self.Level)
+        print(rest, n, cycles)
+        return cycles * self.ProdTime
 
     def set_lvl(self, lvl):
         self.Level = lvl
-        self.EndOfLife = self.end_of_life
 
     def upgrade(self):
         self.set_lvl(self.Level + 1)
@@ -49,28 +67,10 @@ class Mine:
     def set_deposit(self, s):
         self.T = now()
         self.Resource.update(s)
-        self.EndOfLife = self.end_of_life
 
     def add_deposit(self, n):
         self.Resource += n
-        t_extra = self.time(n)
-        info(f'You added {t_extra} of life time')
-        self.EndOfLife = self.end_of_life
-
-    @property
-    def end_of_life(self):
-        return (self.T + self.Resource.N / self.Level * self.ProdTime).replace(microsecond=0)
-
-    @property
-    def time_left(self):
-        return None if self.Paused else self.EndOfLife - now()
-
-    def update(self):
-        tdiff = now() - self.T
-        n_cycles = int(tdiff / self.ProdTime)
-        self.T += n_cycles * self.ProdTime
-        if not self.Paused:
-            self.Resource -= n_cycles * self.Level
+        info(f'You added {self.extra_time(n)} of life time')
 
     def set_status(self, status: bool):
         self.Paused = status
@@ -82,7 +82,7 @@ class Mine:
 
     @property
     def data(self):
-        return [self.Resource, self.Level, self.Resource.N, self.time_left, ['ON', 'OFF'][self.Paused]]
+        return [self.Resource, self.Level, self.Resource.N, self.time_left_str, ['ON', 'OFF'][self.Paused]]
 
 
 class Mines:
@@ -102,7 +102,8 @@ class Mines:
 
     def __add__(self, other: Mine):
         self.L.append(other)
-        self.reload()
+        self.L = sorted(self.L)
+        self.save()
 
     @property
     def size(self):
